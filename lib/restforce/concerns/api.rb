c126f252bbdd39326592b984216c2519f7685935
Emily McSherry <emily@litify.com>
2019-06-13 12:16:59 -0400

    hello byebug world

--- lib/restforce/concerns/api.rb
+++ lib/restforce/concerns/api.rb
@@ -184,6 +184,7 @@ module Restforce
       # Restforce.configuration.mashify is false.
       def query(soql)
         response = api_get 'query', q: soql
+        byebug
         mashify? ? response.body : response.body['records']
       end
 
